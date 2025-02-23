d0a62d503b74e86ee76aebf866673a40d9f37ce9
Omkarkulkarni7 <kulkarniomkar2003@gmail.com>
2025-02-23 13:51:16 +0530

    fixed issues

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,6 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  output: "export", // Enable static export for GitHub Pages
-  distDir: "out", // Export files to 'out' folder
-  images: { unoptimized: true }, // Disable image optimization for GitHub Pages
-  trailingSlash: true, // Ensures proper routing on GitHub Pages
-  basePath: "", // No extra repo name in the URL
-  assetPrefix: "/", // Fix asset loading issues
 };
 
 export default withMDX(nextConfig);
